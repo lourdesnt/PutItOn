4a9adc36c82693f226d7e5725619f4855f662e8b
lourdes <lourdes_navarrotocon@nervion.salesianas.org>
2022-02-16 19:10:13 +0100

    fixed errors

--- app/src/main/java/com/example/putiton/Menu.java
+++ app/src/main/java/com/example/putiton/Menu.java
@@ -8,8 +8,10 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TabHost;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 import basededatos.AdminSQLiteOpenHelper;
 import clases.Producto;
@@ -29,12 +31,21 @@ public class Menu extends AppCompatActivity {
 
         TabHost.TabSpec spec = tab_apartados.newTabSpec("Mujer");
         spec.setContent(R.id.mujer);
-        spec.setIndicator("Mujer");
+        if(Locale.getDefault().getLanguage().equals("en")) {
+            spec.setIndicator("Woman");
+        } else {
+            spec.setIndicator("Mujer");
+        }
+
         tab_apartados.addTab(spec);
 
         spec = tab_apartados.newTabSpec("Hombre");
         spec.setContent(R.id.hombre);
-        spec.setIndicator("Hombre");
+        if(Locale.getDefault().getLanguage().equals("en")) {
+            spec.setIndicator("Man");
+        } else {
+            spec.setIndicator("Hombre");
+        }
         tab_apartados.addTab(spec);
 
     }
